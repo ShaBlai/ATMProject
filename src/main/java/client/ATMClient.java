8b7c44d0cd0a3342a30bb145817abc47e320a49e
Juan Cruz <juan.cruz@tlgcohort.com>
2022-11-14 10:13:41 -0800

    updated atm client menu

--- src/main/java/client/ATMClient.java
+++ src/main/java/client/ATMClient.java
@@ -16,6 +16,8 @@ public class ATMClient extends Bank {
     private static final double savings = 6;
     private static final int Exit = 7;
 
+    private double balance;
+
     Scanner scan = new Scanner(System.in);
 
     public ATMClient() {
@@ -24,34 +26,50 @@ public class ATMClient extends Bank {
 
 
 
-    /*while (true)
+    while(true)
+
     {
-        System.out.println("Select " +Withdrawal+ " to withdraw");
-        System.out.println("Select " +Deposit+ " to deposit");
-        System.out.println("Select " + Balance_Inquiry+ " to check balance inquiry");
-        System.out.println("Select " +Transfer+ " to transfer funds");
-        System.out.println("Select "+Checking+ " to check checking");
-        System.out.println("select "+savings+ "to check savings");
-        System.out.println("Select " +Exit+ "to Exit");
+        System.out.println("Select " + Withdrawal + " to withdraw");
+        System.out.println("Select " + Deposit + " to deposit");
+        System.out.println("Select " + Balance_Inquiry + " to check balance inquiry");
+        System.out.println("Select " + Transfer + " to transfer funds");
+        System.out.println("Select " + Checking + " to check checking");
+        System.out.println("select " + savings + "to check savings");
+        System.out.println("Select " + Exit + "to Exit");
         int options = scan.nextInt();
 
-        switch(options)
-        {
+        switch (options) {
             case 1:
-            System.out.println("Enter amount you would like to withdraw");
-                int withdraw = scan.nextInt();
-                if(getUserData().)
+                System.out.println("Enter amount you would like to withdraw");
+                double withdraw = scan.nextDouble();
+                if (balance >= withdraw) {
+                    balance -= withdraw;
+                } else {
+                    System.out.println("Insufficient Balance");
+                }
+                break;
+
+            case 2:
+                System.out.println("Enter the amount you want to deposit");
+                double deposit = scan.nextDouble();
+                balance += deposit;
+                System.out.println("Deposit successful your current balance is " + balance);
+                break;
+
+            case 3:
+                System.out.println("Balance: " + balance);
+                break;
+
+            case 4:
+                System.out.println("Choose bank account type: ");
+                if (options == Checking) {
+                    System.out.println("select amount you would like to transfer:");
+                    double transferAmount = scan.nextDouble();
+                } else {
+
+                }
 
 
         }
     }
-
-    public Scanner getScan() {
-        return scan;
-    }
-
-    public void setScan(Scanner scan) {
-        this.scan = scan;
-    }
-*/
 }
