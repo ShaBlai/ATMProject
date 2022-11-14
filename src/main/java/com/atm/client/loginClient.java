8d674889a5acfef4f789cd13392816980fdf954c
ShaBlai <Shawn.Blair@tlgcohort.com>
2022-11-14 10:59:45 -0800

    Added login client logic

--- src/main/java/com/atm/client/loginClient.java
+++ src/main/java/com/atm/client/loginClient.java
@@ -3,25 +3,53 @@ import java.util.Scanner;
 
 public class loginClient {
 
-    String userNameInput;
+    static String userNameInput;
     Integer cardInput;
     Integer pinInput;
 
-    /*public boolean verifyUserInfo(){
+    /*public boolean verifyUserInfo() {
+        if
 
+        return true;
     }*/
+
+
 
     public static void main(String[] args) {
 
         Scanner myObj = new Scanner(System.in);
-        String userName;
+        Integer cardNumberUserProvided;
+        Integer pinNumberUserProvided;
 
         // Enter username and press Enter
-        System.out.println("Enter username");
-        userName = myObj.nextLine();
+       // System.out.println("Please enter your card number, ending in the last 4 digits");
+        //cardNumberUserProvided = myObj.nextInt();
 
-        System.out.println("Username is: " + userName);
+        do {
+            System.out.println("Please enter your 4 digit card number!");
+            while (!myObj.hasNextInt()) {
+                System.out.println("That's not the correct format for a card number!");
+                myObj.next(); // this is important!
+            }
+            cardNumberUserProvided = myObj.nextInt();
+        } while (cardNumberUserProvided <= 1000 || cardNumberUserProvided>=9999);
+
+        System.out.println("Thank you! This your card number " + cardNumberUserProvided);
+
+        do {
+            System.out.println("Please enter your 4 digit pin!");
+            while (!myObj.hasNextInt()) {
+                System.out.println("That's not the correct format for a pin!");
+                myObj.next(); // this is important!
+            }
+            pinNumberUserProvided = myObj.nextInt();
+        } while (pinNumberUserProvided <= 1000 || pinNumberUserProvided>=9999);
+
+        System.out.println("Thank you! You have entered the correct pin format " + pinNumberUserProvided);
+
+
     }
+
 
 
 }
