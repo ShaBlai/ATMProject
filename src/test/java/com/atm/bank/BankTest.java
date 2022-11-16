3a27ae22c4f0506a583f297105f8b5d1eb94c882
Cameron Davis <csd@fastmail.com>
2022-11-16 11:09:56 -0500

    added BankTest to git

---
+++ src/test/java/com/atm/bank/BankTest.java
@@ -0,0 +1,44 @@
+package com.atm.bank;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BankTest {
+
+    @Test
+    public void verifyLoginClientInfo() {
+    }
+
+    @Test
+    public void withdrawFromSavings() {
+    }
+
+    @Test
+    public void withdrawFromChecking() {
+    }
+
+    @Test
+    public void depositToSavings() {
+    }
+
+    @Test
+    public void depositToChecking() {
+    }
+
+    @Test
+    public void transferFromChecking() {
+    }
+
+    @Test
+    public void transferFromSavings() {
+    }
+
+    @Test
+    public void displayCustomerCheckingBalance() {
+    }
+
+    @Test
+    public void displayCustomerSavingBalance() {
+    }
+}
